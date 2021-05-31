77ffae921a4b42423ec6ccbd1dfb2300d97a21e0
hello-ty <ty.gb3150.wwshz@gmail.com>
2021-05-31 16:56:46 +0900

    ✨ firebase実装

---
+++ src/components/fire.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRcFcjz0NHIAGBJiERvC355Ga53eJR-Sc",
+  authDomain: "ref-app-b5198.firebaseapp.com",
+  projectId: "ref-app-b5198",
+  storageBucket: "ref-app-b5198.appspot.com",
+  messagingSenderId: "1043952573570",
+  appId: "1:1043952573570:web:47677c39c44dd7045b289b",
+  measurementId: "G-CDF9SS05SC",
+};
+
+if (firebase.apps.length == 0) {
+  firebase.initializeApp(firebaseConfig);
+}
