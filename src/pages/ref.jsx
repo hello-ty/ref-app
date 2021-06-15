9bbe75890d9e05b717d75ce067b6e9f6a99b3d70
hello-ty <ty.gb3150.ss@gmail.com>
2021-06-15 20:52:15 +0900

    ♻️

--- src/pages/ref.jsx
+++ src/pages/ref.jsx
@@ -4,6 +4,7 @@ import "src/components/fire"
 import firebase from "firebase"
 import clsx from "clsx"
 import Layout from "src/components/Layout"
+import EditModal from "src/components/Ref/EditModal"
 
 const db = firebase.firestore()
 
@@ -50,50 +51,6 @@ export default function Ref() {
     return setFlag(true)
   }, [])
 
-  const handlePlus = useCallback(() => {
-    setCount((prevCount) => prevCount + 1)
-  }, [count])
-
-  const handleSub = useCallback(() => {
-    if (count > 0) {
-      setCount((prevCount) => prevCount - 1)
-    }
-  }, [count])
-
-  const doAction = (e) => {
-    if (count > 0) {
-      const ob = {
-        food_name: dish.name,
-        food_quantity: count,
-        food_genre: dish.genre,
-        food_unit: dish.unit,
-        food_url: dish.url,
-      }
-      foods.map((d, i) => {
-        if (d.name === ob.food_name) {
-          db.collection("stocks").doc(d.id).delete()
-        }
-      })
-      db.collection("stocks")
-        .add(ob)
-        .then((ref) => {
-          // router.reload();
-          setCh(!ch)
-          setFlag(!flag)
-        })
-    }
-  }
-
-  const doDelete = (e) => {
-    db.collection("stocks")
-      .doc(e.id)
-      .delete()
-      .then((ref) => {
-        setCh(!ch)
-        setFlag(!flag)
-      })
-  }
-
   return (
     <Layout setNow={setNow} now={now} title="冷蔵庫" bgcolor="blue">
       <section className="text-gray-600 body-font">
@@ -130,65 +87,16 @@ export default function Ref() {
           </div>
         </div>
       </section>
-      {flag ? (
-        <>
-          <div
-            className={clsx(
-              "text-gray-600 body-font container w-24",
-              classes.modal
-            )}
-          >
-            <div className="p-6">
-              <img
-                style={{ height: "70px", display: "block" }}
-                src={dish.url}
-                alt="null"
-                className="h-20 rounded w-full object-cover object-center mb-6"
-              />
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                {dish.name}
-              </h2>
-              <div className="container mb-5">
-                <span className="text-2xl bg-blue-200 py-2 px-3 rounded-full">
-                  {count}
-                </span>
-                <span className="text-2xl p-2">{dish.unit}</span>
-              </div>
-              <div className="container mb-3">
-                <button
-                  onClick={handlePlus}
-                  className=" mx-auto mr-2 text-gray-600 bg-blue-200 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg"
-                >
-                  +1
-                </button>
-                <button
-                  onClick={handleSub}
-                  className=" mx-auto text-gray-600 bg-blue-200 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg"
-                >
-                  -1
-                </button>
-              </div>
-              <div className="container mb-3">
-                <button
-                  onClick={() => doDelete(dish)}
-                  className=" mx-auto mr-2 text-gray-600 bg-blue-200 border-0 py-2 px-3 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-base"
-                >
-                  全て捨てる
-                </button>
-                <button
-                  onClick={doAction}
-                  className=" mx-auto text-gray-600 bg-blue-200 border-0 py-2 px-3 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-base"
-                >
-                  個数を変更
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className={classes.back} onClick={() => setFlag(!flag)}></div>
-        </>
-      ) : (
-        ""
-      )}
+      <EditModal
+        dish={dish}
+        flag={flag}
+        setFlag={setFlag}
+        setCount={setCount}
+        ch={ch}
+        setCh={setCh}
+        count={count}
+        foods={foods}
+      />
     </Layout>
   )
 }
